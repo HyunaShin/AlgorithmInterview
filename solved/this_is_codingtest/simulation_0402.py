acacfd21c6c8c11cee0209fec8d6f56fec8fc237
has <has@wemakeprice.com>
2021-08-19 20:44:02 +0900

    제한시간 3분 초과, ord함수를 자주 쓰자

--- solved/this_is_codingtest/simulation_0402.py
+++ solved/this_is_codingtest/simulation_0402.py
@@ -4,8 +4,6 @@ def main(position):
     y = [1,2,3,4,5,6,7,8]
     # RU, RD, DL, DR, LU, LD, UL, UR
     movements = [(2,-1), (2,1), (-1,2), (1,2), (-2,-1), (-2,1), (-1,-2), (1,-2)]
-    print([i for i in enumerate(x)])
-    print([i for i in enumerate(y)])
 
     dx = list(filter(lambda x : x[-1] == position[0], enumerate(x)))[0][0]
     dy = list(filter(lambda x : x[-1] == int(position[1]), enumerate(y)))[0][0]
@@ -14,6 +12,7 @@ def main(position):
     for move in movements:
         if (dx + move[0] >= 0 )and (dy + move[1] >=0) and (dx + move[0] <= 8) and (dx + move[0] <= 8):
             cases +=1
+
     return cases
 
 if __name__ == "__main__":
