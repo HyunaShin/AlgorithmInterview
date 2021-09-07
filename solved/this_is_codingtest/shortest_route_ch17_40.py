519359ea4bee0c0d89d4ae720c7fb969c60d02b3
has <has@wemakeprice.com>
2021-09-07 21:30:03 +0900

    제한시간 40분, 30분컷. 맨처음에 출발 노드 정할 떄 출발노드의 최단거리 테이블 초기화하는것 잊지 말것

---
+++ solved/this_is_codingtest/shortest_route_ch17_40.py
@@ -0,0 +1,33 @@
+import sys
+import heapq
+
+INF = int(1e9)
+input = sys.stdin.readline
+n,m = map(int, input().rstrip().split(" "))
+#최단거리 테이블 초기화
+distance = [INF] * (n +1)
+
+#그래프 정보 받아오기(양방향 고려)
+graph = [[] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    x,y = map(int, input().rstrip().split(" "))
+    graph[x].append((1,y))
+    graph[y].append((1,x))
+
+q = []
+#1번노드에서 출발하고, 출발은 비용이 들지 않는다.
+heapq.heappush(q, (0,1))
+distance[1] = 0
+
+while q:
+    dist, node = heapq.heappop(q)
+    if dist > distance[node]:
+        continue
+    for n_dist, n_node in graph[node]:
+        cost = n_dist + dist
+        if cost < distance[n_node]:
+            distance[n_node] = cost
+            heapq.heappush(q, (n_dist,n_node))
+
+max_dist = max(list(filter(lambda x : x != INF,distance)))
+print( distance.index(max_dist), " ", max_dist, " ",distance.count(max_dist))
