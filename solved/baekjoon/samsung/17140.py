8eec6f1a3453c82b6afb09593d582110378d0e78
has <has@wemakeprice.com>
2021-10-04 14:47:58 +0900

    while문 조건 진짜 잘보자

--- solved/baekjoon/samsung/17140.py
+++ solved/baekjoon/samsung/17140.py
@@ -1,4 +1,49 @@
 
+
+def solution():
+    from collections import Counter
+    r,c,k = map(int, input().split())
+    A = [list(map(int, input().split())) for _ in range(3)]
+
+    def R():
+        copy_a = []
+        max_len =0
+        for tmp in A:
+            tmp = Counter(tmp)
+            # print(tmp)
+            del (tmp[0])
+            copy_tmp = sorted(tmp.items(), key = lambda x : (x[1], x[0]))
+            tt = []
+            for a in copy_tmp:
+                tt.extend(a)
+                if len(tt) == 100:
+                    break
+            max_len = max(max_len, len(tt))
+            copy_a.append(tt)
+        for ss in copy_a:
+            ss += [0] * (max_len - len(ss))
+        return copy_a
+
+    time = 0
+    while True:
+        if time > 100:
+            break
+        if r <= len(A) and c<= len(A[0]) and A[r-1][c-1] ==k:
+            break
+        if len(A) >= len(A[0]):
+            A = R()
+        else:
+            #How to transpose : A를 열별로 묶고 풀어주기
+            A = list(map(list, zip(*A)))
+            A = R()
+            A = list(map(list, zip(*A)))
+        time +=1
+    if time > 100:
+        print(-1)
+    else:
+        print(time)
+
+
 def sort(set_arr, cnt_arr):
     if len(cnt_arr) <= 1:
         return set_arr, cnt_arr
@@ -30,7 +75,7 @@ if __name__ == "__main__":
     result = 0
     x_len, y_len = 3, 3
     while True:
-        if result == 100:
+        if result > 100:
             print(-1)
             break
         try:
