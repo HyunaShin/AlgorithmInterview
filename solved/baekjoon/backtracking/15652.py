ee2bed65e1341c73bfa0111d7845c986c0a54cf0
has <has@wemakeprice.com>
2021-10-11 14:59:52 +0900

    백트래킹 할 때 넘기는 파라미터로만 상태 트리를 갱신하는것도 방법

--- solved/baekjoon/backtracking/15652.py
+++ solved/baekjoon/backtracking/15652.py
@@ -1,24 +1,18 @@
+
 n,m = map(int, input().split())
 result = []
-visited= set()
 def dfs(result):
-    if len(result) == m :
-        print(" ".join(map(str, result)))
-        return result
+    #제일 효율적인 소스(백트래킹이긴함)
+    if len(result) == m:
+        print(" ".join(result))
+        #리턴값이 없으니, m까지 오면 result array는 이전 상태 트리에 있는 값들만.
+        return
 
-    for i in range(1, n+1):
-        if len(result):
-            if result[-1] <= i:
-                result.append(i)
-                result = dfs(result)
-                if tuple(result) not in visited:
-                    visited.add(tuple(result))
-                result.pop()
-        else:
-            result.append(i)
-            result = dfs(result)
-            if tuple(result) not in visited:
-                visited.add(tuple(result))
-            result.pop()
-    return result
+    for i in range(1,n+1):
+        # 건너뛸 조건만 넣어주면 소스가 심플해짐
+        if result and i < int(result[-1]):
+            continue
+        #지금까지의 상태 트리에 있는 값만 메모리에 올려둠
+        dfs(result + [str(i)])
+
 dfs(result)
