f3d34ee3c88eeeebc9c3243ae9488d2db1feb3bf
has <has@wemakeprice.com>
2021-10-11 17:45:56 +0900

    5:35분컷

---
+++ solved/baekjoon/backtracking/15654.py
@@ -0,0 +1,14 @@
+n,m = map(int, input().split())
+numbers = [int(i) for i in input().split()]
+numbers.sort()
+
+def dfs(result):
+    if len(result) == m:
+        print(" ".join(result))
+        return
+    for num in numbers:
+        if result and str(num) in result:
+            continue
+        dfs(result + [str(num)])
+    return
+dfs([])
