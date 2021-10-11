d65e1e87e833fd75ced7ac2a26c230c8258f6d62
has <has@wemakeprice.com>
2021-10-11 18:39:00 +0900

    6:38 솔

---
+++ solved/baekjoon/backtracking/15655.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+
+def dfs(result, trial):
+    if trial == m+1:
+        print(" ".join(map(str, result)))
+        return
+    for val in arr:
+        if result and result[-1] >= val:
+            continue
+        dfs(result + [val], trial+1)
+    return
+
+dfs([], 1)
